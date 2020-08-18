accf4eddc797f08972b91ea0da8808ef478011ed
july-young <1103225671@qq.com>
2020-08-18 09:24:03 +0800

    feat : add gateway

---
+++ july-gateway/src/main/java/com/july/mall/gateway/GatewayApp.java
@@ -0,0 +1,18 @@
+package com.july.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: july
+ * @date: 2020/8/17 14:31
+ * @description:
+ */
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
